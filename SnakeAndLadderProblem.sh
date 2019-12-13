c48a6b1ba90ec5abce70042477aac6381358b6e2
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-13 17:44:17 +0530

    Added : switch case to check if it's noPlay, ladder or snake

--- SnakeAndLadderProblem.sh
+++ SnakeAndLadderProblem.sh
@@ -5,4 +5,11 @@ echo "WELCOME TO SNAKE AND LADDER GAME"
 #variable
 position=0
 diceValue=$(((RANDOM%6)+1))
-echo $diceValue
+optionsValue=$(((RANDOM%3)+1))
+
+case $optionsValue in
+	1)position=$position;;
+	2)position=$(($position+$diceValue));;
+	3)position=$(($position-$diceValue));;
+esac
+echo $position
