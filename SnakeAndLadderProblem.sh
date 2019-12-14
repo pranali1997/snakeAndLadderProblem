a87885427cbba580024d1671d64d510c6b0d71c5
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-14 11:37:56 +0530

    Refactored : changed name of function from getOnePlayer to getPositionValue

--- SnakeAndLadderProblem.sh
+++ SnakeAndLadderProblem.sh
@@ -4,12 +4,13 @@ echo "WELCOME TO SNAKE AND LADDER GAME"
 
 #constant
 MAX_POSITION=100
+
+#variable
 position=0
 
-function getOnePlayer()
+function getPositionValue()
 {
 	position=$1
-
 	optionsValue=$(((RANDOM%3)+1))
 	dieValue=$(((RANDOM%6)+1))
 
@@ -24,45 +25,51 @@ function getOnePlayer()
 		position=0
 	fi
 
-	if [ $position -gt 100 ]
+	if [ $position -gt $MAX_POSITION ]
 	then
 		position=$(($position-$dieValue))
 	fi
+
 	echo  $position
+
 }
 
 function getWinner()
 {
-	getOnePlayer
 
-	position1=0
-	position2=0
-	flag=1
-	while [ $position1 -lt 100 ] && [ $position2 -lt 100 ]
+	playerPosition1=0
+	playerPosition2=0
+	nextPlayer=1
+	dieCount=0
+
+	while [ $playerPosition1 -lt $MAX_POSITION ] && [ $playerPosition2 -lt $MAX_POSITION ]
 	do
-		if [ $flag -eq 1 ]
+		if [ $nextPlayer -eq 1 ]
 		then
-			position1=$(getOnePlayer $position1)
-			if [ $position1 -eq 100 ]
+			playerPosition1=$( getPositionValue $playerPosition1)
+			echo "player1 position: "$playerPosition1
+			if [ $playerPosition1 -eq $MAX_POSITION ]
 			then
 				echo "Player1 wins"
 			else
-				flag=0
+				nextPlayer=0
 			fi
 			dieCount=$(($dieCount+1))
+
 		else
-			position2=$(getOnePlayer $position2)
-			if [ $position2 -eq 100 ]
+			playerPosition2=$( getPositionValue $playerPosition2)
+			echo "player2 position: "$playerPosition2
+			if [ $playerPosition2 -eq $MAX_POSITION ]
 			then
 				echo "Player2 wins"
 			else
-				flag=1
+				nextPlayer=1
 			fi
 		fi
+
 		dieCount=$(($dieCount+1))
-		echo "player1 position: " $position1
-		echo "player2 position: "$position2
 	done
+
 	echo "number of times die was rolled :" $dieCount
 }
 getWinner
